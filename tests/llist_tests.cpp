75fab2f184466724dc1dce12c48451245022a46a
Bacal Mesfin <mesfinb2@vcu.edu>
2022-09-29 18:22:31 -0400

    modified cmakelists to export symbols in windows dll

--- tests/llist_tests.cpp
+++ tests/llist_tests.cpp
@@ -3,13 +3,13 @@ extern "C"{
   #include "llist.h"
 }
 
-TEST(llist_create,llist_create_not_null){
+TEST(llist_create,create_not_null){
   llist* list = llist_create();
   ASSERT_FALSE(list==NULL);
   llist_delete(&list);
 }
 
-TEST(llist_add,llist_add_not_null){
+TEST(llist_add,add_not_null){
   llist* list = llist_create();
   int a = 32;
   llist_add(list,(void*)&a, sizeof(int));
@@ -18,45 +18,45 @@ TEST(llist_add,llist_add_not_null){
   llist_delete(&list);
 }
 
-TEST(llist_get,llist_get_valid){
+TEST(llist_get,get_valid){
   llist* list = llist_create();
   int a = 32;
   llist_add(list, (void*)&a, sizeof(int));
   ASSERT_TRUE(list->head->data == llist_get(list,0));
   llist_delete(&list);
 }
-TEST(llist_get,llist_get_out_of_range){
+TEST(llist_get,get_out_of_range){
   llist* list = llist_create();
   int a = 32;
   llist_add(list, (void*)&a, sizeof(int));
   ASSERT_TRUE(llist_get(list,3) == NULL);
 }
 
-TEST(llist_get,llist_get_on_empty_list){
+TEST(llist_get,get_on_empty_list){
   llist* list = llist_create();
   ASSERT_TRUE(llist_get(list,0) == NULL);
   llist_delete(&list);
 }
 
-TEST(llist_remove,llist_remove_on_empty_list){
+TEST(llist_remove,remove_on_empty_list){
   llist* list = llist_create();
   ASSERT_TRUE(llist_remove(list,0) == -1);
 }
 
-TEST(llist_remove,llist_remove_on_list_of_size_1){
+TEST(llist_remove,remove_on_list_of_size_1){
   llist* list = llist_create();
   int a = 32;
   llist_add(list,(void*)&a, sizeof(int));
   llist_remove(list,0);
   ASSERT_TRUE(list->size == 0);
-  ASSERT_TRUE(list->head == NULL);
+  ASSERT_TRUE(list->head == nullptr);
   llist_delete(&list);
 }
 
-TEST(llist_delete, llist_delete_on_NULL_list){
-  llist* list = NULL;
+TEST(llist_delete, delete_on_NULL_list){
+  llist* list = nullptr;
   llist_delete(&list);
-  ASSERT_TRUE(list == NULL);
+  ASSERT_TRUE(list == nullptr);
 }
 
 int main(int argc, char** argv){
